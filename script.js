9f06dbe2fa5926ca0386f52c08323de80ea3efd1
Ahmed <ahmedhatem.aez@gmail.com>
2024-11-13 21:23:52 +0200

    Add main design for the calculator

--- script.js
+++ script.js
@@ -14,6 +14,17 @@ function divide(firstNum,secondNum){
     return firstNum / secondNum
 }
 
+function operate(first,second,operator){
+    if(operator == "+")
+        return add(first,second)
+    if(operator == "-")
+        return subtract(first,second)
+    if(operator == "*")
+        return multiply(first,second)
+    if(operator == "/")
+        return divide(first,second)
+}
+
 let firstNum;
 let secondNum;
 let operator;
