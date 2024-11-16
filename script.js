4db832ff6b6fed1826a9a81743ef3fd16f46c2fc
Ahmed <ahmedhatem.aez@gmail.com>
2024-11-16 14:27:09 +0200

    Display operators and operands

--- script.js
+++ script.js
@@ -24,23 +24,51 @@ function operate(first,second,operator){
     if(operator == "/")
         return divide(first,second)
 }
-let el;
 
 let display = document.getElementById("display");
-
-function addEvent(btn){
-    for (let i = 0, len = btn.length; i < len; i++) {
-        btn[i].addEventListener("click", () =>  display.textContent = btn[i].value);
-    }
-}
-let firstNum = document.querySelectorAll("button");
-addEvent(firstNum)
-
-console.log(el);
-
-console.log("hello")
-let secondNum;
+display.textContent = "0";
+let firstNum = "";
+let secondNum = "";
 let operator;
+let flag = false;
+
+function addEvent1(btn){
+    display.textContent = "";
+    for (let i = 0, len = btn.length; i < len; i++) {
+        btn[i].addEventListener("click", () =>  {
+            display.textContent += btn[i].value;
+            
+        });
+    }
+    
+}
+
+function addEvent2(btn){
+    
+    for (let i = 0, len = btn.length; i < len; i++) {
+        btn[i].addEventListener("click", () =>  {
+            btn[i].style.backgroundColor = "red";
+            firstNum = display.textContent;
+            secondClick(firstNum,btn[i].value);
+            display.textContent = "";
+        });
+    }
+    
+}
+function secondClick(first,sign){
+
+    console.log(first);
+    console.log(sign);
+}
+
+let events1 = document.querySelectorAll(".operand");
+let events2 = document.querySelectorAll(".operator");
+
+addEvent1(events1);
+addEvent2(events2);
+
+
+
 
 
 
